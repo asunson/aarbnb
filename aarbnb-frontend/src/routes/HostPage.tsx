41d713aaefdd9d16e3aa66fd890874fc015348f4
asunson <aaronsunson@gmail.com>
2024-09-01 19:16:02 -0400

    move host page to shadcn and remove react-bootstrap and sass

--- aarbnb-frontend/src/routes/HostPage.tsx
+++ aarbnb-frontend/src/routes/HostPage.tsx
@@ -1,39 +1,49 @@
-import { DateTime } from 'luxon';
-import React, { useEffect, useState } from 'react';
-import { Card } from 'react-bootstrap';
-import { useServicesContext } from '../context/ServicesContext';
-import { AppRequest } from '../types';
+import { DateTime } from "luxon";
+import React, { useEffect, useState } from "react";
+import { useServicesContext } from "../context/ServicesContext";
+import { AppRequest } from "../types";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "../components/ui/card";
 
 export const HostPage: React.FC = () => {
-    const { requestService } = useServicesContext();
-    const [appRequests, setAppRequests] = useState<AppRequest[]>([]);
+  const { requestService } = useServicesContext();
+  const [appRequests, setAppRequests] = useState<AppRequest[]>([]);
 
-    useEffect(() => {
-        requestService.getRequests().then(
-            requests => setAppRequests(requests)
-        )
-            .catch(e => console.log(e))
-    }, [requestService])
+  useEffect(() => {
+    requestService
+      .getRequests()
+      .then((requests) => setAppRequests(requests))
+      .catch((e) => console.log(e));
+  }, [requestService]);
 
-    return <>
-        {appRequests.map(a => <div key={a.id}>
-            <Card className='mbs'>
-                <Card.Body>
-                    <Card.Title>
-                        {a.subject}
-                    </Card.Title>
-                    <Card.Subtitle>
-                        {formatTimestamp(a.timestamp)}
-                    </Card.Subtitle>
-                    <Card.Text>
-                        {a.description}
-                    </Card.Text>
-                </Card.Body>
-            </Card>
-        </div>)}
-    </>
-}
+  return (
+    <div className="flex flex-col max-h-screen overflow-auto">
+      {appRequests.map((a) => (
+        <div key={a.id}>
+          <Card>
+            <CardHeader>
+              <CardTitle>{a.subject}</CardTitle>
+              <CardDescription>
+                {a.user}-{formatTimestamp(a.timestamp)}
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <p>{a.description}</p>
+            </CardContent>
+          </Card>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 const formatTimestamp = (timestamp?: number): string => {
-    return DateTime.fromMillis(timestamp ?? 0).toLocaleString(DateTime.DATETIME_MED)
-}
+  return DateTime.fromMillis(timestamp ?? 0).toLocaleString(
+    DateTime.DATETIME_MED
+  );
+};
